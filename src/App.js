65e6b2a97929720ab0e75840bbe16e00a22a7f91
tutumimo <r-tsutsumimoto8270@ts.kyoto-art.ac.jp>
2020-07-08 12:07:37 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ToDoListItem from "./ToDoListItem.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <form
+         className="App-form"
+         >
+           <div>
+             <input
+             id="title"
+             placeholder="title"
+             />
+             <textarea
+             id="description"
+             placeholder="description"
+             />
+           </div>
+        <div>
+          <button
+           type="submit"
+          >
+            登録
+          </button>
+        </div>
+       </form>
+       <div>
+          <ToDoListItem
+            title="ホームページ作成"
+            description="サークルホームページを今週中に作成する"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
